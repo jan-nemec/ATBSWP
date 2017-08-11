a750dfa9c4b22bbf9de2e0168699e3ce3fe27590
Jan Nemec <jan.nemec@live.com>
2017-08-11 14:46:50 +0200

    Chapter 2...

---
+++ 02_for_five_times.py
@@ -0,0 +1,13 @@
+# In code, a for statement looks something like for i in range(5):
+# and always includes the following:
+
+# The for keyword
+# A variable name
+# The in keyword
+# A call to the range() method with up to three integers passed to it
+# A colon
+# Starting on the next line, an indented block of code (called the for clause)
+
+print('My name is')
+for i in range(5):
+    print('Jan Five Times (' + str(i) + ')')
