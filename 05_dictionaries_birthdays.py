8df85953dbe403fd2a58b7f2a485ed7298ea5f4a
Jan Nemec <jan.nemec@live.com>
2017-09-11 14:44:31 +0200

    Chapter 5 ...

---
+++ 05_dictionaries_birthdays.py
@@ -0,0 +1,16 @@
+birthdays = {'Alice': 'Apr 1', 'Bob': 'Dec 12', 'Carol': 'Mar 4'}
+
+while True:
+    print('Enter a name: (blank to quit)')
+    name = input()
+    if name == '':
+        break
+
+    if name in birthdays:
+        print(birthdays[name] + ' is the birthday of ' + name)
+    else:  # if not, you can add it using the same square bracket syntax combined with the assignment operator
+        print('I do not have birthday information for ' + name)
+        print('What is their birthday?')
+        bday = input()
+        birthdays[name] = bday
+        print('Birthday database updated.')
