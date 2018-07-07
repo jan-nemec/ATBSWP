2163bd4f866d3984225ac195cfbfb62af28572f5
Jan Nemec <jan.nemec@live.com>
2018-07-07 20:48:53 +0200

    Updated LPTHW 2018/07/07

---
+++ 07_project_phone_and_email.py
@@ -0,0 +1,45 @@
+# phone_and_email.py - Finds phone numbers and email addresses on the clipboard
+
+import pyperclip  # import pyperclip, re
+import re
+
+phoneRegex = re.compile(r'''(
+    (\d{3}|\(\d{3}\))?              # area code
+    (\s|-|\.)?                      # separator
+    (\d{3})                         # first 3 digits
+    (\s|-|\.)                       # separator
+    (\d{4})                           # last 4 digits
+    (\s*(ext|x|ext.)\s*\d{2,5})?    # extension
+)''', re.VERBOSE)
+
+
+emailRegex = re.compile(r'''(
+    [a-zA-Z0-9._%+-]+   # username
+    @                   # @ symbol
+    [a-zA-Z0-9.-]+      # domain name
+    (\.[a-zA-Z]{2,4})   # dot-something
+)''', re.VERBOSE)
+
+# Find matches in the clipboard text
+text = str(pyperclip.paste())
+matches = []
+
+for groups in phoneRegex.findall(text):
+    phoneNum = '-'.join([groups[1], groups[3], groups[5]])
+    if groups[6] != '':
+        phoneNum += ' x' + groups[6]
+    matches.append(phoneNum)
+    # matches.append(groups[0])
+
+for groups in emailRegex.findall(text):
+    matches.append(groups[0])
+
+# Copy results to the clipboard
+if len(matches) > 0:
+    # The pyperclip.copy() function takes only a single string value, not a list of strings,
+    # so you call the join() method on matches.
+    pyperclip.copy('\n'.join(matches))
+    print('Copied to the clipboard:')
+    print('\n'.join(matches))
+else:
+    print('No phone numbers or email addresses found.')
